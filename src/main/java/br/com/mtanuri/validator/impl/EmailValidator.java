0c3a941df274a97d5323b822e55cf2591d7fec33
Lothar <lothar223@hotmail.com>
2024-11-18 21:19:27 -0300

    email validation

--- src/main/java/br/com/mtanuri/validator/impl/EmailValidator.java
+++ src/main/java/br/com/mtanuri/validator/impl/EmailValidator.java
@@ -3,11 +3,23 @@ package br.com.mtanuri.validator.impl;
 import br.com.mtanuri.validator.Validator;
 
 public class EmailValidator implements Validator {
+
+    String mail;
+
     public EmailValidator(String mail) {
+        this.mail = mail;
     }
 
     @Override
     public boolean validate() {
-        return false;
+
+        if (this.mail == null){
+            return false;
+        }
+
+        if (!this.mail.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")){
+            return false;
+        }
+        return true;
     }
 }

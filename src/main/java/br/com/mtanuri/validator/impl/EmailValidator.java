0f1d22210b52d69d134c0500162005e2dc679fc5
Iago Teles <iagooteles@gmail.com>
2024-11-18 21:10:24 -0300

    added email validator

--- src/main/java/br/com/mtanuri/validator/impl/EmailValidator.java
+++ src/main/java/br/com/mtanuri/validator/impl/EmailValidator.java
@@ -2,12 +2,25 @@ package br.com.mtanuri.validator.impl;
 
 import br.com.mtanuri.validator.Validator;
 
+import java.util.regex.Pattern;
+
 public class EmailValidator implements Validator {
-    public EmailValidator(String mail) {
+    private static final String EMAIL_REGEX =
+            "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
+    private final String email;
+
+    public EmailValidator(String email) {
+        this.email = email;
     }
 
     @Override
     public boolean validate() {
-        return false;
+        if (email == null || email.isEmpty()) {
+            return false;
+        }
+        return EMAIL_PATTERN.matcher(email).matches();
     }
 }
